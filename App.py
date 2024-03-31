97a82e83c0d975e2bc8b666f0bfaa7e820c4b67e
prashanth-1012 <165567782+prashanth-1012@users.noreply.github.com>
2024-03-31 20:10:02 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,9 @@
 import streamlit as st
-name = st.text_input('Enter the name of the friend whom you want wish')
+name = st.text_input('Enter the of the friend whom you want wish')
 st.title("HBD wishes")
 
 def HBD(name):
-  str.write(f"Happy Birthday {name}. May god bless you with abundance")
+  return(f"Happy Birthday {name}. May god bless you with abundance")
+
+wish = HBD(name)
+st.write(wish)
