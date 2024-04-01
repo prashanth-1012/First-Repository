e9e08a806c33d70e50b6ed1c07325b2febf7ca2f
prashanth-1012 <165567782+prashanth-1012@users.noreply.github.com>
2024-04-01 13:55:05 +0530

    Update App.py

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ name = st.text_input('Enter the of the friend whom you want wish')
 st.title("HBD wishes")
 
 def HBD(name):
-  return(f"Happy Birthday {name}. May god bless you with abundance")
+  return(f"Happy Birthday {name}. \n May god bless you with abundance")
 
 wish = HBD(name)
 st.write(wish)
